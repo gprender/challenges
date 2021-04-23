3f48d7c8948dd4441bacddd74eb182bfc03beb9f
gprender <graemeprendergast@gmail.com>
2021-04-22 21:45:39 -0700

    codewars: Beeramid

---
+++ cpp/beeramid.cpp
@@ -0,0 +1,18 @@
+/**
+ * Given a sum of money (bonus) and the price for a can of beer, 
+ * determine the tallest square beercan pyramid you can build.
+ * 
+ * Nothing too crazy here, just determining the largest sum of
+ * squares contained within some number (bonus/price).
+ */
+
+#include <iostream>
+
+int beeramid(int bonus, double price) {
+  int remaining = bonus / price;
+  int layer = 1;
+  for (; remaining >= 0; layer++) {
+    remaining -= (layer * layer);
+  }
+  return layer-2;
+}
